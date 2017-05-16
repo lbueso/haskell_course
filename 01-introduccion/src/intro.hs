c3c9b0f84db095187ce679af6b57a01e7f65abdc
Ignacio Ballesteros <ignacioballesterosgonzalez@gmail.com>
2017-05-17 00:14:39 +0200

    Ejercicio Intro

--- 01-introduccion/src/intro.hs
+++ 01-introduccion/src/intro.hs
@@ -3,17 +3,17 @@
 {-
   Define el tipo de la función "incrementar"
 -}
--- incrementar :: Int -> Int
+
 incrementar x = 1 + x
 
 {-
   Implementa la función "mul" y define el tipo de la función.
 -}
--- mul :: Int -> Int -> Int
-mul x y = x * y
+
+mul = undefined
 
 {-
   Implementa la función "duplicar" en base a "mul"
 -}
--- duplicar :: Int -> Int
-duplicar = mul 2
+
+duplicar = undefined
