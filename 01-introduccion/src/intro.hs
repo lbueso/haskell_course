dee3a976fbb09134d9eb6b88fab3c1738c7af049
Ignacio Ballesteros <ignacioballesterosgonzalez@gmail.com>
2017-05-17 00:12:51 +0200

    Some exercises

---
+++ 01-introduccion/src/intro.hs
@@ -0,0 +1,19 @@
+
+
+{-
+  Define el tipo de la función "incrementar"
+-}
+-- incrementar :: Int -> Int
+incrementar x = 1 + x
+
+{-
+  Implementa la función "mul" y define el tipo de la función.
+-}
+-- mul :: Int -> Int -> Int
+mul x y = x * y
+
+{-
+  Implementa la función "duplicar" en base a "mul"
+-}
+-- duplicar :: Int -> Int
+duplicar = mul 2
